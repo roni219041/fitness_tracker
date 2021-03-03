62c8e4f618c840d3ce896984f144e14462133b4f
Martin <mirander73@gmail.com>
2021-03-03 20:35:19 +0200

    initial

---
+++ FitnessTracker/Models/BodyPart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FitnessTracker.Models
+{
+    public partial class BodyPart
+    {
+        public BodyPart()
+        {
+            Excercises = new HashSet<Excercise>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Excercise> Excercises { get; set; }
+    }
+}
---
+++ FitnessTracker/Models/Excercise.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FitnessTracker.Models
+{
+    public partial class Excercise
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? UserId { get; set; }
+        public int? BodypartId { get; set; }
+        public int? CategoryId { get; set; }
+
+        public virtual BodyPart Bodypart { get; set; }
+        public virtual Category Category { get; set; }
+        public virtual User User { get; set; }
+    }
+}
