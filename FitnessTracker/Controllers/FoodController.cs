5066d7355656bb597f8a4555eb22a512aaabdf34
Martin <mirander73@gmail.com>
2021-03-15 00:24:57 +0200

    add pre-added food implementation

--- FitnessTracker/Controllers/FoodController.cs
+++ FitnessTracker/Controllers/FoodController.cs
@@ -26,6 +26,63 @@ namespace FitnessTracker.Controllers
             return View(foodData);
         }
 
+        public IActionResult AddFood()
+        {
+            CreateFoodViewModel inputFoods = new CreateFoodViewModel();
+            List<Food> allFoods = _db.Foods.OrderBy(f => f).ToList();
+            inputFoods.allFoods = allFoods;
+            inputFoods.uniqueFoods = allFoods.Select(f => f.Name).Distinct().OrderBy(f => f).ToList();
+            return View("FoodCreate", inputFoods);
+        }
+
+        public IActionResult AddFoodAction(Food food)
+        {
+            _db.Foods.Add(food);
+            _db.SaveChanges();
+            return RedirectToAction("RenderMain", "Dashboard");
+        }
+
+        public IActionResult AddPreAddedFood(PreAddedFoodViewModel inputData)
+        {
+            string userId = _db.Users.FirstOrDefault(u => u.UserName == User.Identity.Name).Id;
+            if (userId == null)
+            {
+                throw new Exception("No Authenticated user could be found!");
+            }
+            int count = inputData.Number;
+            string foodName = inputData.Name;
+            Food foodInfo = _db.Foods.FirstOrDefault(f => f.Name == foodName);
+            if (foodInfo == null)
+            {
+                throw new Exception("Food could not be found!");
+            }
+            if (count <= 0)
+            {
+                throw new Exception("Count is below or equal to zero!");
+            }
+
+            if (count > 15)
+            {
+                throw new Exception("Count number too big!");
+            }
+            for (int i = 1; i <= count; i++)
+            {
+                Food foodEntity = new Food()
+                {
+                    Calories = foodInfo.Calories,
+                    Carbohydrates = foodInfo.Carbohydrates,
+                    DateAdded = DateTime.Now,
+                    Fat = foodInfo.Fat,
+                    Name = foodInfo.Name,
+                    Protein = foodInfo.Protein,
+                    UserId = userId
+                };
+                _db.Foods.Add(foodEntity);
+            }
+            _db.SaveChanges();
+            return RedirectToAction("RenderMain", "Dashboard");
+        }
+
         public List<FoodDataViewModel> GetAllUserFoods()
         {
             var user = _db.Users.FirstOrDefault(u => User.Identity.Name == u.UserName);
@@ -48,6 +105,11 @@ namespace FitnessTracker.Controllers
                 });
             }
             return foodData;
+        }
+
+        public IActionResult CreateCustomFood(AddCustomFoodViewModel foodInputData)
+        {
+
         }
     }
 }
---
+++ FitnessTracker/ViewModels/AddCustomFoodViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessTracker.ViewModels
+{
+    public class AddCustomFoodViewModel
+    {
+        [Required] public string Name { get; set; }
+        [Required] public int Calories { get; set; }
+        [Required] public int Fat { get; set; }
+        [Required] public int Protein { get; set; }
+        [Required] public int Carbohydrates { get; set; }
+    }
+}
